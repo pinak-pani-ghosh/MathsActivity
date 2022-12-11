505fa7465e11d928d944968d8d9a41970baacf69
pinak pani ghosh <51861201+pinak-pani-ghosh@users.noreply.github.com>
2022-12-11 16:58:55 +0530

    Update Activity3.py

--- Activity3.py
+++ Activity3.py
@@ -1,11 +1,24 @@
+# activity 3
+# to genetate the fibonacci sequence using user inp
+
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 n=int(input("Enter a limit: "))
-print(a)
-print(b)
-x=1
-for x in range(3, n+1):
-        if x<=n:
-            a,b=b,a+b
-            print(b)
-            x=+1
+
+def recur_fibo(n):
+    if n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+
+# check if the number of terms is valid
+if n <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(n):
+       print(recur_fibo(i))
+       v
